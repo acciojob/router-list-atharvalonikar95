aea2782d15f69d33accf4b3e7b10dc50c289f3a1
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-05-28 20:40:48 +0530

    Pass items prop to ItemList and ItemDetail components

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,30 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import ItemDetail from './ItemDetail';
 import ItemList from './ItemList';
 const App = () => {
+      const items = [
+        {
+            id: 1,
+            name: "Item 1",
+            description: "Description for Item 1",
+        },
+        {
+            id: 2,
+            name: "Item 2",
+            description: "Description for Item 2",
+        },
+        {
+            id: 3,
+            name: "Item 3",
+            description: "Description for Item 3",
+        },
+    ];
   return (
     <div>
         {/* Do not remove the main div */}
         <BrowserRouter>
           <Routes>
-            <Route path={`/`} element={<ItemList/>}></Route>
-            <Route path={`/items/:id`} element={<ItemDetail/>}></Route>
+            <Route path={`/`} element={<ItemList items={items}/>}></Route>
+            <Route path={`/items/:id`} element={<ItemDetail items={items} />}></Route>
           </Routes>
       </BrowserRouter>
     </div>
