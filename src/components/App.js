267d17596a2d572c40593803042d77693984e7a3
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-05-28 20:37:52 +0530

    Add routing for ItemList and ItemDetail components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import ItemDetail from './ItemDetail';
+import ItemList from './ItemList';
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <BrowserRouter>
+          <Routes>
+            <Route path={`/`} element={<ItemList/>}></Route>
+            <Route path={`/items/:id`} element={<ItemDetail/>}></Route>
+          </Routes>
+      </BrowserRouter>
     </div>
   )
 }
